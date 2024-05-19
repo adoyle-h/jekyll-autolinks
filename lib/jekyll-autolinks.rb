bb7c60884b48b060a5453f7267af18360a08b5c0
ADoyle <adoyle.h@gmail.com>
2024-05-20 03:11:10 +0800

    fix: site is missing

--- lib/jekyll-autolinks.rb
+++ lib/jekyll-autolinks.rb
@@ -2,11 +2,9 @@ require 'jekyll'
 require 'rinku'
 
 Jekyll::Hooks.register :pages, :post_render do |page|
-  if not page.html?
-    return
+  if page.html?
+    link_attr = page.site.config['autolinks']['link_attr']
+    skip_tags = page.site.config['autolinks']['skip_tags']
+    page.content = +Rinku.auto_link(page.content, :all, link_attr, skip_tags)
   end
-
-  link_attr = site.config['autolinks']['link_attr']
-  skip_tags = site.config['autolinks']['skip_tags']
-  page.content = +Rinku.auto_link(page.content, :all, link_attr, skip_tags)
 end
