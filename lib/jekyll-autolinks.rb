25917389eeccc24516f9b3d37b38d7d98b22204d
ADoyle <adoyle.h@gmail.com>
2024-05-21 06:31:39 +0800

    fix

--- lib/jekyll-autolinks.rb
+++ lib/jekyll-autolinks.rb
@@ -2,9 +2,7 @@ require 'jekyll'
 require 'rinku'
 
 Jekyll::Hooks.register :pages, :post_render do |page|
-  if page.html?
-    link_attr = page.site.config['autolinks']['link_attr']
-    skip_tags = page.site.config['autolinks']['skip_tags']
-    page.content = +Rinku.auto_link(page.content, :all, link_attr, skip_tags)
-  end
+  link_attr = page.site.config['autolinks']['link_attr']
+  skip_tags = page.site.config['autolinks']['skip_tags']
+  page.output = Rinku.auto_link(page.output, :all, link_attr, skip_tags)
 end
