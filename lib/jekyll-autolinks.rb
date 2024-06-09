76ef6adc285f1f5bb1f223d1439cd7e18d4fb3ae
ADoyle <adoyle.h@gmail.com>
2024-06-09 13:03:18 +0800

    fix: do not convert links in assets/js/search-data.json

--- lib/jekyll-autolinks.rb
+++ lib/jekyll-autolinks.rb
@@ -1,7 +1,7 @@
 require 'jekyll'
 require 'rinku'
 
-Jekyll::Hooks.register :pages, :post_render do |page|
+Jekyll::Hooks.register :posts, :post_render do |page|
   link_attr = page.site.config['autolinks']['link_attr']
   skip_tags = page.site.config['autolinks']['skip_tags']
   page.output = Rinku.auto_link(page.output, :all, link_attr, skip_tags)
