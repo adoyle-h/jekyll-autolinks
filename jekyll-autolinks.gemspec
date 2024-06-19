973cd869171e0970c5a5b3f67e3d5f3f68bc0fb4
ADoyle <adoyle.h@gmail.com>
2024-06-20 05:13:47 +0800

    fixup 76ef6adc

--- jekyll-autolinks.gemspec
+++ jekyll-autolinks.gemspec
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 Gem::Specification.new do |spec|
   spec.name          = "jekyll-autolinks"
-  spec.version       = "0.2.3"
+  spec.version       = "0.2.4"
   spec.authors       = ["ADoyle"]
   spec.email         = ["adoyle.h@gmail.com"]
   spec.summary       = "Autolink for your Jekyll site."
--- lib/jekyll-autolinks.rb
+++ lib/jekyll-autolinks.rb
@@ -1,8 +1,10 @@
 require 'jekyll'
 require 'rinku'
 
-Jekyll::Hooks.register :posts, :post_render do |page|
-  link_attr = page.site.config['autolinks']['link_attr']
-  skip_tags = page.site.config['autolinks']['skip_tags']
-  page.output = Rinku.auto_link(page.output, :all, link_attr, skip_tags)
+Jekyll::Hooks.register :pages, :post_render do |page|
+  if page.html?
+    link_attr = page.site.config['autolinks']['link_attr']
+    skip_tags = page.site.config['autolinks']['skip_tags']
+    page.output = Rinku.auto_link(page.output, :all, link_attr, skip_tags)
+  end
 end
